8d2af082a858b559c833f3b6e26c0a219f2a37f4
edmalfizeo <edmalfizeo@gmail.com>
2025-01-15 13:06:01 -0300

    Updating Cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   // Ativar validação global
   app.useGlobalPipes(new ValidationPipe());
 
